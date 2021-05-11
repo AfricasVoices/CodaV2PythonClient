3714f8d2d957ddef3120e6f00fc2098094659184
DanielMwendwa <danielmwendwam@gmail.com>
2021-05-11 17:37:00 +0300

    func that gets dataset ids

--- coda_v2_python_client/firebase_client_wrapper.py
+++ coda_v2_python_client/firebase_client_wrapper.py
@@ -36,3 +36,22 @@ class CodaV2Client:
         cred = credentials.Certificate(crypto_token_path)
         app = firebase_admin.initialize_app(cred, name=app_name)
         return cls(firestore.client(app))
+
+    def get_dataset_ids():
+        """
+        Gets ids of all the datasets excluding their segements.
+
+        :return: Ids of all datasets excluding their segements
+        :rtype: set of str
+        """
+        segment_ids = get_segment_ids()
+        assert len(segment_ids) == len(set(segment_ids)), "Segment ids not unique"
+
+        dataset_ids = set(segment_ids)
+        for dataset_id in get_segmented_dataset_ids():
+            segment_count = get_segment_count(dataset_id)
+            if segment_count is not None and segment_count > 1:
+                for segment_index in range(2, segment_count + 1):
+                    dataset_ids.remove(id_for_segment(dataset_id, segment_index))
+
+        return dataset_ids
