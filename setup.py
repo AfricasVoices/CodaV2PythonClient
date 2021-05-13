a1296ca7d73fccdc2854819591685c675c14ba97
DanielMwendwa <danielmwendwam@gmail.com>
2021-05-13 14:18:01 +0300

    packages the module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="CodaV2PythonClient",
+    version="0.1.0",
+    url="https://github.com/AfricasVoices/CodaV2PythonClient",
+    packages=["coda_v2_python_client"],
+    install_requires=["firebase_admin", "coredatamodules @ git+https://github.com/AfricasVoices/CoreDataModules"]
+)
