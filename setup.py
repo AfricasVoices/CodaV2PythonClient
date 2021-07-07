228d0f6901d0832b457dc4c863e35ec38848cb57
DanielMwendwa <danielmwendwam@gmail.com>
2021-07-07 07:25:57 +0300

    Version bump for release v0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="CodaV2PythonClient",
-    version="0.0.0",
+    version="0.1.0",
     url="https://github.com/AfricasVoices/CodaV2PythonClient",
     packages=["coda_v2_python_client"],
     install_requires=["firebase_admin", "coredatamodules @ git+https://github.com/AfricasVoices/CoreDataModules"]
