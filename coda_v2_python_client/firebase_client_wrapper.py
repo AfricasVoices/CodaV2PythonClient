e3d4a7a707ff1b8b04958d752d491d98656b563d
DanielMwendwa <danielmwendwam@gmail.com>
2021-07-07 07:09:40 +0300

    access msg attribute without converting it to dict first

--- coda_v2_python_client/firebase_client_wrapper.py
+++ coda_v2_python_client/firebase_client_wrapper.py
@@ -254,13 +254,7 @@ class CodaV2Client:
             messages_ref = messages_ref.where("LastUpdated", "<=", last_updated_before)
         raw_messages = [message.to_dict() for message in messages_ref.get(transaction=transaction)]
 
-        messages = []
-        for message in raw_messages:
-            if "LastUpdated" in message:
-                message["LastUpdated"] = message["LastUpdated"].isoformat(timespec="microseconds")
-            messages.append(message)
-
-        return [Message.from_firebase_map(message) for message in messages]
+        return [Message.from_firebase_map(message) for message in raw_messages]
 
     def get_dataset_message(self, dataset_id, message_id, transaction=None):
         """
@@ -298,7 +292,7 @@ class CodaV2Client:
                                    field and where the LastUpdated field is later than last_updated_after. Defaults to None.
         :type last_updated_after: datetime, optional
         :return: Messages in this dataset, filtered by 'LastUpdated' timestamp if requested.
-        :rtype: list of dict
+        :rtype: list of core_data_modules.data_models.message.Message
         """
         segment_count = self.get_segment_count(dataset_id)
         if segment_count == 1:
@@ -316,12 +310,11 @@ class CodaV2Client:
             dataset_last_updated = None
             for segment_messages in messages_by_segment.values():
                 for msg in segment_messages:
-                    msg = msg.to_dict()
-                    if "LastUpdated" in msg:
-                        if dataset_last_updated is None or msg["LastUpdated"] > dataset_last_updated:
-                            dataset_last_updated = msg["LastUpdated"]
-                        if dataset_first_updated is None or msg["LastUpdated"] < dataset_first_updated:
-                            dataset_first_updated = msg["LastUpdated"]
+                    if hasattr(msg, "last_updated"):
+                        if dataset_last_updated is None or msg.last_updated > dataset_last_updated:
+                            dataset_last_updated = msg.last_updated
+                        if dataset_first_updated is None or msg.last_updated < dataset_first_updated:
+                            dataset_first_updated = msg.last_updated
 
             # Check all the segments for any messages between the latest one we fetched above and the most recently updated
             # message seen in any segment. If we didn't fetch any new messages for a segment, use the oldest timestamp
@@ -331,10 +324,9 @@ class CodaV2Client:
             for segment_id, segment_messages in messages_by_segment.items():
                 segment_last_updated = None
                 for msg in segment_messages:
-                    msg = msg.to_dict()
-                    if "LastUpdated" in msg:
-                        if segment_last_updated is None or msg["LastUpdated"] > segment_last_updated:
-                            segment_last_updated = msg["LastUpdated"]
+                    if hasattr(msg, "last_updated"):
+                        if segment_last_updated is None or msg.last_updated > segment_last_updated:
+                            segment_last_updated = msg.last_updated
 
                 if segment_last_updated is None:
                     segment_last_updated = dataset_first_updated
