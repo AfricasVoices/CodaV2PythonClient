ea04f366b621534c9f94d4d42975fe65db337517
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-11-01 13:26:58 +0300

    rearrange code

--- coda_v2_python_client/firebase_client_wrapper.py
+++ coda_v2_python_client/firebase_client_wrapper.py
@@ -858,8 +858,8 @@ class CodaV2Client:
             msg_to_delete = Message.from_firebase_map(message_snapshot.to_dict())
             msg_to_delete_metrics = self.compute_segment_messages_metrics(segment_id, [msg_to_delete], transaction=transaction)
 
-            messages_metrics, msg_to_delete_metrics = messages_metrics.to_firebase_map(), message_snapshot.to_dict()
             updated_messages_metrics = dict()
+            messages_metrics, msg_to_delete_metrics = messages_metrics.to_firebase_map(), message_snapshot.to_dict()
             for key in messages_metrics.keys():
                 updated_messages_metrics[key] = messages_metrics[key] - msg_to_delete_metrics[key]
             
